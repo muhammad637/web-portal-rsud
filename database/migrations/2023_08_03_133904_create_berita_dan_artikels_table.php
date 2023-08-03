19ec8f228f01bab718c71bc980577b51c34088eb
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-08-03 21:18:42 +0700

    pembuatan model migration serta controller dari beritadanartikel dan kategori serta relasinya

---
+++ database/migrations/2023_08_03_133904_create_berita_dan_artikels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita_dan_artikels', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug');
+            $table->string('isi');
+            $table->string('video')->nullable();
+            $table->string('gambar');
+            $table->enum('jenis',['berita', 'artikel']); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita_dan_artikels');
+    }
+};
