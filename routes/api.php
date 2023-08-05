2598e475f97c244dd657d1ee8e38ba5c77c60c74
muhammad637 <muhammadfaridmustakim@gmail.com>
2023-08-05 09:58:14 +0700

    menambahkan tabel rawat-inap dan layanan dan menambahkan backend kategoricontroller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/kategori', [KategoriController::class,'index']);
+Route::post('/kategori/create', [KategoriController::class,'store']);
+Route::put('/kategori/update/{kategori}', [KategoriController::class,'update'])->name('kategori.update');
+Route::get('/kategori/{nama_kategori}', [KategoriController::class,'show'])->name('kategori.show');
