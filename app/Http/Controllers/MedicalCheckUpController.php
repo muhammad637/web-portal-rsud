3d71dd74c8a906764f7981a8e8db13873970c6b1
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-08-09 09:03:46 +0700

    mcu e kelalen

---
+++ app/Http/Controllers/MedicalCheckUpController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MedicalCheckUp;
+use Illuminate\Http\Request;
+
+class MedicalCheckUpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MedicalCheckUp  $medicalCheckUp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MedicalCheckUp $medicalCheckUp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MedicalCheckUp  $medicalCheckUp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MedicalCheckUp $medicalCheckUp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MedicalCheckUp  $medicalCheckUp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MedicalCheckUp $medicalCheckUp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MedicalCheckUp  $medicalCheckUp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MedicalCheckUp $medicalCheckUp)
+    {
+        //
+    }
+}
