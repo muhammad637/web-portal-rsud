9a3164b8f499be75317e7e5692ddc421f1bdb708
Ilhamrsdi <muhamadilhamrosidi0707@gmail.com>
2023-09-12 07:49:05 +0700

    ktt

--- resources/views/pages/pasien-pengunjung/ketersediaan-kamar.blade.php
+++ resources/views/pages/pasien-pengunjung/ketersediaan-kamar.blade.php
@@ -1,7 +1,7 @@
 @extends('main')
 @section('content')
 <section class="home-slider owl-carousel">
-      <div class="slider-item bread-item" style="background-image: url('{{asset('images/bg1.jpg')}}');" data-stellar-background-ratio="0.5">
+      <div class="slider-item bread-item" style="background-image: url('{{asset('images/bg1.webp')}}');" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container" data-scrollax-parent="true">
           <div class="row slider-text align-items-end">
@@ -17,7 +17,7 @@
         <div class="container">
             <br>
             <div class="card-total">
-                <p class="total" style="color: #169BE6">TOTAL TEMPAT TIDUR : <span class="text-dark">1.766</span></p> 
+                <p class="total" style="color: #169BE6">TOTAL TEMPAT TIDUR : <span class="text-dark">320</span></p> 
             </div>
             <br>
             <table class="table table-bordered text-center">
@@ -30,45 +30,107 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="td-color">BANGSAL 8 LT.1</td>
-                        <td>KLS1</td>
-                        <td>xxx</td>
+                        <td class="td-color">ICU</td>
+                        <td></td>
+                        <td>12</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 7 LT.1</td>
-                        <td>KLS2</td>
-                        <td>xxx</td>
+                        <td class="td-color">ICCU</td>
+                        <td></td>
+                        <td>12</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 6 LT.2</td>
-                        <td>KLS3</td>
-                        <td>xxx</td>
+                        <td class="td-color">Sayu Wiwit (Ibu)</td>
+                        <td>Kelas I, Kelas II, Kelas III</td>
+                        <td>23</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 5 LT.2</td>
-                        <td>KLS3</td>
-                        <td>xxx</td>
+                        <td class="td-color">Sayu Wiwit (Rawat Bayi Gabung)</td>
+                        <td>Kelas I, Kelas II, Kelas III</td>
+                        <td>17</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 4 LT.3</td>
-                        <td>KLSVIP</td>
-                        <td>xxx</td>
+                        <td class="td-color">Mas Alit</td>
+                        <td>Kelas I, Kelas II, Kelas III</td>
+                        <td>30</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 3 LT.3</td>
-                        <td>KLSVIP</td>
-                        <td>xxx</td>
+                        <td class="td-color">Sekardalu</td>
+                        <td>Kelas III, Itensif (NICU 10)</td>
+                        <td>15</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 2 LT.3</td>
-                        <td>KLSVVIP</td>
-                        <td>xxx</td>
+                        <td class="td-color">Tawang Alun Atas</td>
+                        <td>Kelas I, Kelas II, Kelas III</td>
+                        <td>27</td>
                     </tr>
                     <tr>
-                        <td class="td-color">BANGSAL 1 LT.3</td>
-                        <td>KLSVVIP</td>
-                        <td>xxx</td>
+                        <td class="td-color">Tawang Alun Bawah</td>
+                        <td>Kelas II, Kelas III</td>
+                        <td>30</td>
                     </tr>
+                    <tr>
+                        <td class="td-color">Prof. dr. Immanoedin</td>
+                        <td>VVIP, VIP</td>
+                        <td>18</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Agung Wilis</td>
+                        <td>Kelas I, Kelas II, Kelas III</td>
+                        <td>38</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Pulmo Center</td>
+                        <td>Kelas I,Kelas II, Kelas III</td>
+                        <td>15</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">TB RO</td>
+                        <td>Kelas III</td>
+                        <td>2</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Isolasi I</td>
+                        <td>Isolasi</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Isolasi II</td>
+                        <td>Isolasi</td>
+                        <td>16</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Isolasi Bayi</td>
+                        <td>Isolasi</td>
+                        <td>8</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Isolasi Bersalin</td>
+                        <td>Isolasi</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">Hemodialisa</td>
+                        <td>Intensif</td>
+                        <td>19</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">IGD</td>
+                        <td>Intensif</td>
+                        <td>17</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">IBS Kamar Operasi</td>
+                        <td>Intensif</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td class="td-color">IBS Recovery Room</td>
+                        <td>Intensif</td>
+                        <td>6</td>
+                    </tr>
+                    
+                    
                     
                 </tbody>
                 
